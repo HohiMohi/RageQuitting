0d354891d2e855361b964837e0c530e3791f9493
AdrianLas <duniacz12@gmail.com>
2024-08-18 21:16:28 +0200

    InputSystem_0.2
    Multiplayer Support:
    - Temporary players spawning function(until issue with Player Input Manager will be fixed)

--- Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -1,28 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.InputSystem.InputAction;
-[RequireComponent(typeof(Rigidbody))]
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody playerRB;
+    [SerializeField]
+    private int playerID;
     private Vector2 movementVector;
     [SerializeField]
-    private float movementSpeed;
+    private float movementSpeed = 5;
+
 
     private void Awake()
     {
         playerRB = GetComponent<Rigidbody>();
     }
 
-    public void OnMove(CallbackContext callbackContext)
+    public void OnMove(InputAction.CallbackContext callbackContext)
     {
         movementVector = callbackContext.ReadValue<Vector2>();
     }
      public void Move(Vector2 movementVector)
     {
-        Debug.Log(movementVector);
+        // Temp version
         Vector2 newPosition = new Vector2(gameObject.transform.position.x + movementVector.x * Time.deltaTime * movementSpeed, gameObject.transform.position.z + movementVector.y * Time.deltaTime * movementSpeed);
         Vector3 newPosition3 = new Vector3(newPosition.x, gameObject.transform.position.y, newPosition.y);
         playerRB.MovePosition(newPosition3);
--- Assets/Scripts/SpawningTest.cs
+++ Assets/Scripts/SpawningTest.cs
@@ -5,10 +5,43 @@ using UnityEngine.InputSystem;
 
 public class SpawningTest : MonoBehaviour
 {
-    public Transform spawningPoint;
+    public GameObject playerPrefab;
+    public int numberOfPlayers = 4;
 
-    public void OnPlayerJoined(PlayerInput input)
+    // Array with control scheme declaration -> use types specified in Input Action Asset
+    public string[]  controlSchemes;
+
+    // Array with used devices -> playerPrefab will be spawned only for active devices
+    public bool[] activeDevices;
+
+    private void OnEnable()
     {
-        Debug.Log(input.playerIndex);
+        // Spawning playerPrefabs for active devices
+        for (int i = 0; i < numberOfPlayers; i++)
+        {
+            // Skip inactive devices
+            if (activeDevices[i] == false) continue;
+
+            switch (controlSchemes[i])
+            {
+                case "Gamepad":
+                    var temp = PlayerInput.Instantiate(playerPrefab, playerIndex: i, controlScheme: controlSchemes[i], pairWithDevice: Gamepad.all[i]);
+                    break;
+
+                case "Keyboard":
+                    temp = PlayerInput.Instantiate(playerPrefab, playerIndex: i, controlScheme: controlSchemes[i]);
+                    break;
+
+                case "Arrows":
+                    temp = PlayerInput.Instantiate(playerPrefab, playerIndex: i, controlScheme: controlSchemes[i]);
+                    // Set current keyboard as used device -> enable playing as 2 players on one keyboard
+                    temp.SwitchCurrentControlScheme("Arrows", Keyboard.current);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
     }
 }
